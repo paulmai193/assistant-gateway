ecb5d4361f1846e1f58b04f841b1d88e9c6ff7f6
Dai Mai <dai.mai@mediastep.com>
2018-05-06 09:09:32 +0700

    remove unused code

--- src/main/java/logia/assistant/gateway/security/DomainUserDetailsService.java
+++ src/main/java/logia/assistant/gateway/security/DomainUserDetailsService.java
@@ -30,21 +30,9 @@ public class DomainUserDetailsService implements UserDetailsService {
     /** The log. */
     private final Logger log = LoggerFactory.getLogger(DomainUserDetailsService.class);
 
-    //    /** The user repository. */
-    //    private final UserRepository userRepository;
-
     /** The credential repository. */
     @Inject
     private CredentialRepository credentialRepository;
-
-    //    /**
-    //     * Instantiates a new domain user details service.
-    //     *
-    //     * @param userRepository the user repository
-    //     */
-    //    public DomainUserDetailsService(UserRepository userRepository) {
-    //        this.userRepository = userRepository;
-    //    }
 
     /* (non-Javadoc)
      * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
@@ -54,36 +42,10 @@ public class DomainUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(final String login) {
         log.debug("Authenticating {}", login);
         String lowercaseLogin = login.toLowerCase(Locale.ENGLISH);
-        //        Optional<User> userByEmailFromDatabase = userRepository.findOneWithAuthoritiesByEmail(lowercaseLogin);
-        //        return userByEmailFromDatabase.map(user -> createSpringSecurityUser(lowercaseLogin, user)).orElseGet(() -> {
-        //            Optional<User> userByLoginFromDatabase = userRepository.findOneWithAuthoritiesByLogin(lowercaseLogin);
-        //            return userByLoginFromDatabase.map(user -> createSpringSecurityUser(lowercaseLogin, user))
-        //                .orElseThrow(() -> new UsernameNotFoundException("User " + lowercaseLogin + " was not found in the " +
-        //                    "database"));
-        //        });
         return this.credentialRepository.findOneWithUserByLogin(login)
                 .map(credential -> createSpringSecurityUser(lowercaseLogin, credential))
                 .orElseThrow(() -> new UsernameNotFoundException("User " + lowercaseLogin + " was not found in the " + "database"));
     }
-
-    //    /**
-    //     * Creates the spring security user.
-    //     *
-    //     * @param lowercaseLogin the lowercase login
-    //     * @param user the user
-    //     * @return the org.springframework.security.core.userdetails. user
-    //     */
-    //    private org.springframework.security.core.userdetails.User createSpringSecurityUser(String lowercaseLogin, User user) {
-    //        if (!user.getActivated()) {
-    //            throw new UserNotActivatedException("User " + lowercaseLogin + " was not activated");
-    //        }
-    //        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
-    //            .map(authority -> new SimpleGrantedAuthority(authority.getName()))
-    //            .collect(Collectors.toList());
-    //        return new org.springframework.security.core.userdetails.User(user.getLogin(),
-    //            user.getPassword(),
-    //            grantedAuthorities);
-    //    }
 
     /**
      * Creates the spring security user.
